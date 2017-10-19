f3e33b915bcf130e3b919c2df3e5e7e10ba1c331
Kevin Harrington <mad.hephaestus@gmail.com>
2017-10-19 17:30:58 -0400

    Adding provisioning mode to Wireless Controller

--- examples/WirelessControllercodeRBE/WirelessControllercodeRBE.ino
+++ examples/WirelessControllercodeRBE/WirelessControllercodeRBE.ino
@@ -19,6 +19,8 @@ int inputCommand = 0;
 byte buttonByte1 = 0;
 byte buttonByte2 = 0;
 unsigned long timer = 0;
+bool provisionTerminal = false;
+byte written=0;
 
 #define virbrationMotorPin 2
 
@@ -27,6 +29,15 @@ void setup() {
 	InitIO();             //Initialize the inputs/outputs and the buffers
 	while (!Serial1) {             //wait for serial to init
 	}
+  DataUpdate();  //read the buttons and the joysticks
+  if(!buttonState[4]){// sheckStart button on boot
+        provisionTerminal = true;
+        
+  }
+  if(provisionTerminal){// sheckStart button on boot
+        Serial.begin(9600); // Serial output begin. Only needed for debug
+        Serial.println("Starting with provision terminal");
+  }
 }
 
 void InitIO() {
@@ -36,13 +47,138 @@ void InitIO() {
 	digitalWrite(virbrationMotorPin, LOW); // Stop shacking of the gamepad
 }
 
+void dump(){
+  int start = millis();
+  
+  while(Serial1.available()<=0){
+    if((millis()-start)>5000){
+      Serial.print("\r\nNO RESPONSE\r\n");
+      return;
+    }
+    //delay(1);
+  }
+  while(Serial1.available()>0){
+      byte val=Serial1.read();
+      if(val>8 && val< 127){
+        written++;
+        Serial.write(val);
+      }
+      //delay(1);
+  }
+}
+
+void write (String s){
+  //written=0;
+  Serial.print("\r\n");
+  Serial.print(s);
+  Serial.print("\r\n");
+  Serial1.print(s);
+}
+
+
+void initializeContolMode(){
+          /*
+       * This section is a secret code: 
+       * ~ 0x0  0x4  0x8  0x1 A  P  e
+       * sent by XCTU to kick the module into AT mode
+       * This needs to be sent before initating AT mode
+       */
+      Serial1.write('~');
+      Serial1.write(0x4);
+      Serial1.write(0x8);
+      Serial1.write(0x1);
+      Serial1.write('A');
+      Serial1.write('P');
+      Serial1.write('e');
+      start();
+      write("ATAP\r");
+      dump();
+      write("ATHV\r");
+      dump();
+      write("ATVR\r");
+      dump();
+      write("ATSH\r");
+      dump();
+      write("ATSL\r");
+      dump();
+      write("ATMY\r");
+      dump();
+      write("ATAC\r");
+      dump();
+      write("ATBD\r");
+      dump();
+      write("ATNB\r");
+      dump();
+      write("ATSB\r");
+      dump();
+      write("ATAP\r");
+      dump();
+      write("ATCN\r");
+      dump();
+}
+
+
+void start(){
+
+      delay(1500);
+      Serial1.write("+++");
+      Serial.write("+++");
+      delay(1000);
+      dump();
+}
+
+void end(){
+      write("ATWR\r");
+      dump();
+      write("ATAC\r");
+      dump();
+      write("ATCN\r");
+      dump();
+}
+
+void provisionTerminalRun(){
+
+  if(Serial.available()>1){
+    String number = Serial.readString();
+      Serial.write("\r\nProvisioning...");
+      Serial.print(number);
+      initializeContolMode();
+     
+      start();
+      write("ATIDB");
+      write(number);
+      write("\r");
+      dump();
+      end();
+      
+      start();
+      write("ATCE0\r");
+      dump();
+      end();
+
+      //Close up connection
+
+      if(written>1){
+        Serial.write("\r\nDone!");
+      }else{
+          Serial.write("\r\nFailed!! module dead..");
+        
+      }
+    provisionTerminal=false;
+  }
+}
+
 void loop() {
 	if (millis() - timer > 25) { // manage the updating freq of all the controlling information
 		// Serial.println("updating and sending");
 		DataUpdate();  //read the buttons and the joysticks
-		printData();   //prints the data and states
+		if(!provisionTerminal)
+		  printData();   //prints the data and states
 		timer = millis();
 	}
+ if(provisionTerminal){
+  provisionTerminalRun();
+ }
 
 }
 
